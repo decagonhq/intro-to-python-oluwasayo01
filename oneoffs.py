68c7456f66db724f1718a1c2a6e20d0c263b73a2
oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-01-09 20:12:57 +0100

    added an asterisk function that returns a tuple of positional arguments, and a dictionary of keyword arguments supplied to it

--- oneoffs.py
+++ oneoffs.py
@@ -9,3 +9,8 @@ def divide(num_1, num_2, floor=True):
         raise TypeError
 
 print(divide(2, 5, False))
+
+def asterisk(*args, **kwargs):
+    return "Positional arguments: {}\nKeyword arguments: {}".format(args, kwargs)
+
+print(asterisk(2, 3, 4, 6, size=(2, 5), name="Oluwasayo", stack="Python"))
